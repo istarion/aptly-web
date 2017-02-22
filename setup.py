c7df95bd2034b613426532e0e416c45c123dfc9a
Sergey Zavgorodniy <s.zavgorodniy@i-dgtl.ru>
2017-02-22 14:44:32 +0300

    v0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(here, 'requirements.txt')) as f:
     requires = f.read()
 setup(
     name='aptlyweb',
-    version=0.4,
+    version=0.5,
     description='Aptly web UI',
     long_description='',
     classifiers=[
