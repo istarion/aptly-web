78690542fbb7fda81f79653c5a8677bca8d00482
Sergey Zavgorodniy <istarion.rus@gmail.com>
2017-05-03 01:08:28 +0300

    global search mock

--- aptlyweb/ng2-static/src/app/global-search/global-search.component.ts
+++ aptlyweb/ng2-static/src/app/global-search/global-search.component.ts
@@ -1,18 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-global-search',
   templateUrl: './global-search.component.html',
-  styleUrls: ['./global-search.component.css']
+  styleUrls: ['./global-search.component.css'],
+  animations: [
+  trigger('flyInOut', [
+    state('in', style({transform: 'translateX(0)'})),
+    transition('void => *', [
+      style({transform: 'translateX(-100%)'}),
+      animate(100)
+    ]),
+    transition('* => void', [
+      animate(100, style({transform: 'translateX(-100%)'}))
+    ])
+  ])
+]
 })
 export class GlobalSearchComponent implements OnInit {
   advancedSearch: Boolean;
+  architectures: Array<object>;
+  filter: object;
+  findTriggered: Boolean;
+  mockRows: Array<object>;
+  mockCols: Array<object>;
 
   constructor() {
     this.advancedSearch = false;
+    this.architectures = [{code: 0, name: "All"}, {code: 1, name: "i386"}, {code: 2, name: "and64"}];
+    this.filter = {
+      arch: 0,
+      versionOperation: '=='
+    };
+    this.mockCols = [
+      { name: 'Container'},
+      { prop: 'name', name: 'Package name'},
+      { name: 'Hash'},
+      { name: 'Version'}
+    ];
+    this.mockRows = [
+      { container: 'Repository: production-messaging', name: 'oracle-5.25-client', hash: 'LDFJlcsvLKJ34LKJ', version: '5.25'},
+      { container: 'Repository: production-messaging', name: 'oracle-5.25-dev', hash: 'LDKFJkOIER23', version: '5.25'},
+      { container: 'Snapshot: production-messaging-001', name: 'oracle-5.25-client', hash: 'LDFJlcsvLKJ34LKJ', version: '5.25'},
+      { container: 'Snapshot: production-messaging-001', name: 'oracle-5.25-dev', hash: 'LDKFJkOIER23', version: '5.25'}
+    ];
   }
 
   ngOnInit() {
+  }
+
+  find() {
+    this.findTriggered = true;
+  }
+
+  searchElementActivate($event) {
+    if ($event.type == 'click') {
+      if ($event.column.prop == 'container') {
+        alert("Go to container page (" + $event.row.container + ")");
+      } else {
+        alert("Go to package page (" + $event.row.name + ")");
+      }
+      console.log($event);
+    }
   }
 
 }
--- aptlyweb/ng2-static/src/app/global-search/global-search.module.ts
+++ aptlyweb/ng2-static/src/app/global-search/global-search.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
-import { MdInputModule, MdButtonModule, MdIconModule, MdSlideToggleModule } from '@angular/material';
+import { MdInputModule, MdButtonModule, MdIconModule, MdSlideToggleModule, MdCheckboxModule,
+  MdSelectModule} from '@angular/material';
 
 import { GlobalSearchComponent } from './global-search.component';
 
@@ -15,7 +19,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
-    MdInputModule, MdButtonModule, MdIconModule, MdSlideToggleModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdInputModule, MdButtonModule, MdIconModule, MdSlideToggleModule, MdCheckboxModule, MdSelectModule,
+    NgxDatatableModule,
     RouterModule.forChild(routes)
   ],
   declarations: [GlobalSearchComponent]
